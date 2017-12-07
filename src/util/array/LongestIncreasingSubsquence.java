0ca76ddba7011c445538cd985b4f25bebe3bcc3f
amruta-kawade <amrutakawade@gmail.com>
2017-12-07 18:22:14 +0530

    NLOGN approach for LIS

--- src/util/array/LongestIncreasingSubsquence.java
+++ src/util/array/LongestIncreasingSubsquence.java
@@ -1,7 +1,5 @@
 package util.array;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class LongestIncreasingSubsquence {
@@ -14,8 +12,7 @@ public class LongestIncreasingSubsquence {
 		}
 		int[] lis = new int[arrLen];
 		initialiseArray(lis);
-		int lisLen = longestIncreasingSubsequence(array,lis);
-		int lisLen1 = longestIncreasingSubsequence(array);
+		int lisLen = longestIncreasingSubsequence(array);
 		System.out.println(lisLen);
 	}
 
@@ -24,57 +21,28 @@ public class LongestIncreasingSubsquence {
 			array[i] = 1;
 		}
 	}
-	//Dynamic Programming Approach
-	private static int longestIncreasingSubsequence(int[] array, int[] lis) {
-		int max = 0;
-		for(int i = 1; i< array.length; i++){
-			for(int j = 0; j < i; j++){
-				if(array[i] > array[j] && lis[i] < (lis[j]+1)){
-					lis[i] = lis[j] + 1;
-					if(lis[i] > max){
-						max = lis[i];
-					}
-				}
-			}
+	private static void print(int[] array, int len){
+		for(int i=0; i < len;i++){
+			System.out.print(array[i]+" ");
 		}
-		return max;
+		System.out.println();
 	}
-	//nlogn approach
-	/*private static int longestIncreasingSubsequence(int[] array) {
-		int n = array.length;
-		List<Integer> lisArray = new ArrayList<Integer>();
-		
-		//Add first element of array to longest subsequence array
-		lisArray.add(array[0]);
-		int lenOfLis = 1;
-		for(int i = 1; i < n; i++){
-			if(array[i] < lisArray.get(0)){
-				lisArray.add(0, array[i]);
-			}else if(array[i] > lisArray.get(n-1)){
-				lisArray.add(array[i]);
-			}else{
-				lisArray.add(binarySearch(lisArray,0,n),array[i]);
-			}
-			lenOfLis++;
-		}
-		return lenOfLis;
-	}*/
-	 private static int ceilIndex(int input[], int T[], int end, int s){
-	        int start = 0;
-	        int middle;
-	        int len = end;
-	        while(start <= end){
-	            middle = (start + end)/2;
-	            if(middle < len && input[T[middle]] < s && s <= input[T[middle+1]]){
-	                return middle+1;
-	            }else if(input[T[middle]] < s){
-	                start = middle+1;
-	            }else{
-	                end = middle-1;
-	            }
-	        }
-	        return -1;
-	    }
+	private static int ceilIndex(int input[], int T[], int end, int s){
+        int start = 0;
+        int middle;
+        int len = end;
+        while(start <= end){
+            middle = (start + end)/2;
+            if(middle < len && input[T[middle]] < s && s <= input[T[middle+1]]){
+                return middle+1;
+            }else if(input[T[middle]] < s){
+                start = middle+1;
+            }else{
+                end = middle-1;
+            }
+        }
+        return end;
+	 }
 	    
 	 public static int longestIncreasingSubsequence(int input[]){
 	        int T[] = new int[input.length];
@@ -84,10 +52,11 @@ public class LongestIncreasingSubsquence {
 	        }
 	        T[0] = 0;
 	        int len = 0;
+	    try{
 	        for(int i=1; i < input.length; i++){
-	            if(input[T[0]] > input[i]){ //if input[i] is less than 0th value of T then replace it there.
+	            if(input[T[0]] >= input[i]){ //if input[i] is less than 0th value of T then replace it there.
 	                T[0] = i;
-	            }else if(input[T[len]] < input[i]){ //if input[i] is greater than last value of T then append it in T
+	            }else if(input[T[len]] <= input[i]){ //if input[i] is greater than last value of T then append it in T
 	                len++;
 	                T[len] = i;
 	                R[T[len]] = T[len-1];
@@ -97,16 +66,19 @@ public class LongestIncreasingSubsquence {
 	                R[T[index]] = T[index-1];
 	            }
 	        }
+	    }catch(Exception e){
+	    	print(T,len);
+	    }
 
 	        //this prints increasing subsequence in reverse order.
-	        System.out.print("Longest increasing subsequence ");
+	        /*System.out.print("Longest increasing subsequence ");
 	        int index = T[len];
 	        while(index != -1) {
 	            System.out.print(input[index] + " ");
 	            index = R[index];
 	        }
 
-	        System.out.println();
+	        System.out.println();*/
 	        return len+1;
 	    }
 }
