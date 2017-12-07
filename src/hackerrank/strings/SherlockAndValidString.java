47cecb59d36c97c7f321fe3d5b94e32135fa9535
amruta-kawade <amrutakawade@gmail.com>
2017-12-07 16:50:01 +0530

    Simplified the code

--- src/hackerrank/strings/SherlockAndValidString.java
+++ src/hackerrank/strings/SherlockAndValidString.java
@@ -1,7 +1,9 @@
 package hackerrank.strings;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -10,9 +12,41 @@ public class SherlockAndValidString {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String input = in.next();
-		
+		int[] charCount = new int[26];
+		if(input.length() == 1){
+			System.out.println("YES");
+        }
+		for(int i=0; i < input.length(); i++){
+			charCount[input.charAt(i) - 'a']++;
+		}
+		Set<Integer> freq = new HashSet<>();
+		int countOfOneFreq = 0;
+		for(int i=0; i < charCount.length; i++){
+			if(charCount[i] != 0){
+				if(charCount[i] == 1){
+					countOfOneFreq++;
+				}else{
+					freq.add(charCount[i]);
+				}
+			} 
+		}
+		//If all characters have same freq countOfOneFreq = 0
+		//If all characters have same freq except one countOfOneFreq = 1
+		if(freq.size() == 1 && countOfOneFreq < 2){
+			System.out.println("YES");
+		}else if(freq.size() == 2 && countOfOneFreq == 0){
+			Integer[] arr = freq.toArray(new Integer[freq.size()]);
+			int diff = Math.abs(arr[1]-arr[0]);
+			if(diff == 1){
+				System.out.println("YES");
+			}else{
+				System.out.println("NO");
+			}
+		}else{
+			System.out.println("NO");
+		}
 		//Charaters vs count map
-		Map<Character,Integer> charMap = new HashMap<Character,Integer>();
+		/*Map<Character,Integer> charMap = new HashMap<Character,Integer>();
 		for(int i=0;i<input.length();i++){
 			if(charMap.containsKey(input.charAt(i))){
 				Integer value = charMap.get(input.charAt(i));
@@ -57,7 +91,7 @@ public class SherlockAndValidString {
 			}
 		}else{
 			System.out.println("NO");
-		}
+		}*/
 	}
 }
 
