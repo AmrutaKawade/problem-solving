fc8cba1b7acf6c5c54cd3b2c17031dff168c268f
amruta-kawade <amrutakawade@gmail.com>
2018-02-02 19:01:17 +0530

    GeeksForGeeks Practice

--- src/util/strings/RemoveCommonChar2Strings.java
+++ src/util/strings/RemoveCommonChar2Strings.java
@@ -1,35 +1,36 @@
 package util.strings;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
+import java.util.Scanner;
+//https://practice.geeksforgeeks.org/problems/remove-common-characters-and-concatenate/0
 public class RemoveCommonChar2Strings {
 	public static void main(String args[]){
-		/*String s1 = "aacdb";
-		String s2 = "gafd";*/
-		String s1 = "abcs";
-        String s2 = "cxzca";
-		String output = removeCommonCharacter(s1, s2);
-		System.out.println(output);
+		Scanner in = new Scanner(System.in);
+		int testCases = in.nextInt();
+		for(int i = 0; i < testCases; i++) {
+			String s1 = in.next();
+			String s2 = in.next();
+			String output = removeCommonCharacter(s1, s2);
+			System.out.println(output);
+		}
 	}
+	//m+n+
 	public static String removeCommonCharacter(String s1,String s2){
-		Map<Character,Integer> charMap = new LinkedHashMap<Character,Integer>();
-		String s = s1.concat(s2);
-		String output = "";
-		for(int i = 0; i < s.length(); i++){
-			if(charMap.containsKey(s.charAt(i))){
-				Integer count = charMap.get(s.charAt(i));
-				count++;
-				charMap.put(s.charAt(i),count);
-			}else{
-				charMap.put(s.charAt(i),1);
+		int[] alphaArray = new int[26];
+		String output2 = "";
+		String output1 = s1;
+		
+		for(int i = 0; i < s1.length(); i++) {
+			alphaArray[s1.charAt(i) - 'a']++;
+		}
+		for(int j = 0; j < s2.length(); j++) {
+			if(alphaArray[s2.charAt(j) - 'a'] != 0) {
+				//remove from the strings 1
+				output1 = output1.replaceAll(String.valueOf(s2.charAt(j)), "");
+			}else {
+				output2 += s2.charAt(j);
 			}
 		}
-		for(Map.Entry<Character, Integer> charCount : charMap.entrySet()){
-			if(charCount.getValue().equals(1)){
-				output += charCount.getKey();
-			}
-		}
-		return output;
+		
+		return (output1+output2).equals("") ? "-1" : output1+output2;
 	}
 }
